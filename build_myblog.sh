02a648d200b3208646f66df930e7110b35c6ed0a
Chokie <chokiezh@gmail.com>
2020-07-22 20:04:20 +0800

    Dir changes.

--- build_myblog.sh
+++ build_myblog.sh
@@ -4,5 +4,5 @@ cd ../blog_ura
 mkdocs.exe build -q
 python add_omote.py
 mv ura ../blog/site
-cp -r ../blog/site/* ../../Chokie-Zhang.github.io
+cp -r ../blog/site/* ../Chokie-Zhang.github.io
 rm -r ../blog/site
