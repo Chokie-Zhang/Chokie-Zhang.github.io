c5ba4e7b1b969eec44667150b72a7b85df1a1469
Chokie <chokiezh@gmail.com>
2020-07-25 23:20:19 +0800

    Update.

---
+++ blog/docs/link/index.md
@@ -0,0 +1,20 @@
+## 个人链接
+
+[Github](https://github.com/Chokie-Zhang)
+
+[Twitter](https://twitter.com/Chokiezh)
+
+## 个人邮箱
+
+chokiezh@gmail.com
+
+chokie@mail.ustc.edu.cn
+
+## 友情链接
+
+[ZHANGWP](https://www.zhangwp.com)
+
+## 鸣谢
+
+本网站的建立参考学习了 [ZHANGWP](https://www.zhangwp.com) 的网站源码。万分感谢 [ZHANGWP](https://www.zhangwp.com) 不厌其烦地在各个领域给予的帮助和指导，他是本人计算机能力发展的指路人。
+
--- blog_ura/add_omote.py
+++ blog_ura/add_omote.py
@@ -6,7 +6,8 @@ ura_dirs = ['.', 'game', 'anime', 'comic', 'music']
 
 for dir_ in ura_dirs:
     for f_name in os.listdir(dir_):
-        with open(os.path.join(dir_, f_name), 'r', encoding='utf8') as f:
-            f_html = f.read()
-        with open(os.path.join(dir_, f_name), 'w', encoding='utf8') as f:
-            f.write(re.sub('omote/', '../', f_html))
+        if f_name[-5:] == '.html':
+            with open(os.path.join(dir_, f_name), 'r', encoding='utf8') as f:
+                f_html = f.read()
+            with open(os.path.join(dir_, f_name), 'w', encoding='utf8') as f:
+                f.write(re.sub('omote/', '../', f_html))
