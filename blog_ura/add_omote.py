b01a1c849176ec7e3f7741082dc265f4692b6db6
Chokie <chokiezh@gmail.com>
2020-07-25 23:06:42 +0800

    Update.

--- blog_ura/add_omote.py
+++ blog_ura/add_omote.py
@@ -5,7 +5,8 @@ os.chdir('ura')
 ura_dirs = ['.', 'game', 'anime', 'comic', 'music']
 
 for dir_ in ura_dirs:
-    with open(os.path.join(dir_, 'index.html'), 'r', encoding='utf8') as f:
-        f_html = f.read()
-    with open(os.path.join(dir_, 'index.html'), 'w', encoding='utf8') as f:
-        f.write(re.sub('omote/', '../', f_html))
+    for f_name in os.listdir(dir_):
+        with open(os.path.join(dir_, f_name), 'r', encoding='utf8') as f:
+            f_html = f.read()
+        with open(os.path.join(dir_, f_name), 'w', encoding='utf8') as f:
+            f.write(re.sub('omote/', '../', f_html))
